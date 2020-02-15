caec53feb90e14558744de0c44c5d43eb370a8e8
Natalie Abreu <nrabreu@usc.edu>
2020-02-15 13:29:31 -0800

    made directories relative

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,22 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const app = require("../app");
+let dir = process.cwd() + "/client";
 
 router.get("/healthcheck", (req, res) => {
   res.send("It works!");
 });
 
 router.get("/profile", (req, res) => {
-  let dir = "/mnt/c/Users/Natalie Abreu/Desktop/treehacks2020/client";
   res.sendFile(path.join(dir + "/profile.html"));
 });
 
 router.get("/members", (req, res) => {
-  let dir = "/mnt/c/Users/Natalie Abreu/Desktop/treehacks2020/client";
   res.sendFile(path.join(dir + "/members.html"));
 });
 
 router.get("/resources", (req, res) => {
   res.send("Resources will be here eventually");
-  // let dir = "/mnt/c/Users/Natalie Abreu/Desktop/treehacks2020/client";
   // res.sendFile(path.join(dir + "/resouces.html"));
 });
 
